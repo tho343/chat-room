0176725dac40f392a3d80be2f4f0310745d3a429
Tran Ho <tranho@Mac-mini.local>
2025-06-23 14:34:02 -0700

    update href for index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -6,5 +6,5 @@ document.getElementById("join-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const userName = document.getElementById("name").value;
   const room = document.getElementById("room").value;
-  window.location.href = `/chat.html?username=${userName}&room=${room}`;
+  window.location.href = `/chat-room/chat.html?username=${userName}&room=${room}`;
 });
